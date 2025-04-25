178728a8dc4567f53df94c356617d8f5ea082e7e
Votre Nom <votre_email@example.com>
2025-04-25 17:43:39 +0200

    Hero

---
+++ src/main.js
@@ -0,0 +1,9 @@
+/** @format */
+
+// Import librarys
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+// Import SCSS
+import "./style.scss";
+import "./scss/hero.scss";
+// ton code JS...
